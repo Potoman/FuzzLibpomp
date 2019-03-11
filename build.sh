b14f93a5db63b0f67987ed7e03b1cc147397fb63
Maxime Girard <lepotoman@msn.com>
2019-03-11 19:49:28 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Copyright 2016 Google Inc. All Rights Reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
+
 . $(dirname $0)/../custom-build.sh $1 $2
 . $(dirname $0)/../common.sh
 
